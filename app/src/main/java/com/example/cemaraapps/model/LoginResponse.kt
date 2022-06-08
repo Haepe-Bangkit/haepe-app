c5388b9431f0f3913e17f0c395379aaf7c82eec0
irsa12 <irsafaiza00@gmail.com>
2022-06-08 17:46:59 +0700

    HeaPe Apps - add family request

--- app/src/main/java/com/example/cemaraapps/model/LoginResponse.kt
+++ app/src/main/java/com/example/cemaraapps/model/LoginResponse.kt
@@ -3,12 +3,11 @@ package com.example.cemaraapps
 import com.google.gson.annotations.SerializedName
 import retrofit2.http.Field
 
-data class LoginResponse(
-    @field:SerializedName("idToken")
-    val idToken: String,
-    @field:SerializedName("userId")
-    val userId: String
-    )
+data class LoginResponse( @field:SerializedName("idToken")
+                          val idToken: String,
+                          @field:SerializedName("userId")
+                          val userId: String
+)
 
 data class FamilyResponse(
 //    @field:SerializedName("statusCode")
@@ -17,8 +16,8 @@ data class FamilyResponse(
 //    val error: String,
 //    @field:SerializedName("message")
 //    val message: String,
-        @field:SerializedName("familyId")
-        val familyId: String
+    @field:SerializedName("familyId")
+    val familyId: String
 //    @field:SerializedName("id")
 //    val id: String
 )
