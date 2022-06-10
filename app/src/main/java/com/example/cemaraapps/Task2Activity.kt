14c62d79e882e813b201039ba2ef23a7b3d180a1
irsa12 <irsafaiza00@gmail.com>
2022-06-10 18:18:07 +0700

    HeaPe Apps - menambahkan fungsi untuk memanggil API di task

--- app/src/main/java/com/example/cemaraapps/Task2Activity.kt
+++ app/src/main/java/com/example/cemaraapps/Task2Activity.kt
@@ -1,18 +1,23 @@
 package com.example.cemaraapps
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.example.cemaraapps.Api.ApiConfig
 import com.example.cemaraapps.databinding.ActivityTask2Binding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Task2Activity : AppCompatActivity() {
     private lateinit var binding: ActivityTask2Binding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTask2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setCreateTask()
 
         binding.apply {
             backTask.setOnClickListener {
@@ -24,4 +29,24 @@ class Task2Activity : AppCompatActivity() {
         }
 
     }
+//    private fun setCreateTask() {
+//        ApiConfig.getApiService().createTask("")
+//            .enqueue(object : Callback<TaskGetResponse> {
+//                override fun onResponse(
+//                    call: Call<TaskGetResponse>,
+//                    response: Response<TaskGetResponse>
+//                ) {
+//                    if (response.isSuccessful) {
+//                        val user = response.body()
+//                        user!!.id.let { Log.d("taskId", it) }
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<TaskGetResponse>, t: Throwable) {
+//                    Log.d(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+//
+//                }
+//
+//            })
+//    }
 }
